3704d12c11b4b3b948dc319c9055997ca076261e
Darcy Laycock <sutto@thefrontiergroup.com.au>
2011-02-03 14:41:48 +0800

    Add in documentation for the rocketpants api client base
    Signed-off-by: John Barker <john@thefrontiergroup.com.au>

--- lib/rocket_pants/client.rb
+++ lib/rocket_pants/client.rb
@@ -1,23 +1,60 @@
 require 'active_support/core_ext/class/attribute'
 require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/string/inflections'
+
+require 'rocket_pants/exceptions'
 
 require 'api_smith'
 require 'will_paginate/collection'
 
 module RocketPants
+  # Implements a generalised base for building clients on top of
+  # the rocket pants controller. This automatically unpacks the API
+  # into the correct response types, handles errors (using the same error registry
+  # as the server) and in general makes it simpler to implement api clients.
+  #
+  # @example A versioned api client
+  #   class MyApiClient < RocketPants::Client
+  #
+  #     class Profile < APISmith::Smash
+  #       property :name
+  #       property :email
+  #     end
+  #
+  #     version 1
+  #     base_uri "http://api.example.com/"
+  #
+  #     def profile
+  #       get 'profile', :transformer => Profile
+  #     end
+  #   end
+  #
   class Client < APISmith::Base
     
     class_attribute :_version, :_actual_endpoint
     
     class << self
       
-      alias _original_endpoint endpoint
-      
+      # @overload version
+      #   @return [Integer] the current API version number for this client
+      # @overload version(number)
+      #   Sets a variable noting what version of the api the client uses and updates
+      #   the endpoint to prefix it with the given version number.
+      #   @param [Integer] number the version of the api to process.
       def version(number = nil)
-        number.nil? ? _version : (self._version = number)
-        endpoint _actual_endpoint
+        if number.nil?
+          _version
+        else
+          self._version = number
+          endpoint _actual_endpoint
+          number
+        end
       end
       
+      alias _original_endpoint endpoint
+
+      # Sets the endpoint url, taking into account the version number as a
+      # prefix if present.
       def endpoint(path)
         self._actual_endpoint = path
         _original_endpoint File.join(*[_version, path].compact.map(&:to_s))
@@ -25,7 +62,11 @@ module RocketPants
       
     end
     
+    # Initializes a new client, optionally setting up the host for this client.
+    # @param [Hash] options general client options
+    # @option options [String] :api_host the overriden base_uri host for this client instance.
     def initialize(options = {})
+      super
       # Setup the base uri if passed in to the client.
       if options[:api_host].present?
         add_request_options! :base_uri => HTTParty.normalize_base_uri(options[:api_host])
@@ -34,8 +75,11 @@ module RocketPants
     
     private
     
-    # Give a response hash, will process it and unpack the
-    # data into the correct data types and wrappers.
+    # Give a response hash from the api, will transform it into
+    # the correct data type.
+    # @param [Hash] response the incoming response
+    # @param [hash] options any options to pass through for transformation
+    # @return [Object] the transformed response.
     def transform_response(response, options = {})
       # Now unpack the response into the data types.
       inner = response.delete("response")
@@ -48,7 +92,12 @@ module RocketPants
       end
     end
     
-    # Returns an API response wrapped in a will_paginate collection.
+    # Returns an API response wrapped in a will_paginate collection
+    # using the pagination key in the response container to set up
+    # the current number of total entries and page details.
+    # @param [Array<Object>] objects the actual response contents
+    # @param [Hash{String => Object}] The response container
+    # @option container [Hash] "pagination" the pagination data to use.
     def paginated_response(objects, container)
       pagination = container.delete("pagination")
       WillPaginate::Collection.create(pagination["current"], pagination["per_page"]) do |collection|
@@ -57,11 +106,22 @@ module RocketPants
       end
     end
     
+    # Finds and uses the transformer for a given incoming object to
+    # unpack it into a useful data type.
+    # @param [Hash, Array<Hash>] object the response object to unpack
+    # @param [Hash] options the unpacking options
+    # @option options [Hash] :transformer,:as the transformer to use
+    # @return The transformed data or the data itself when no transformer is specified.
     def unpack(object, options = {})
       transformer = options[:transformer] || options[:as]
       transformer ? transformer.call(object) : object
     end
     
+    # Processes a given response to check for the presence of an error,
+    # either by the response not being a hash (e.g. it is returning HTML instead
+    # JSON or HTTParty couldn't parse the response).
+    # @param [Hash, Object] response the response to check errors on
+    # @raise [RocketPants::Error] a generic error when the type is wrong, or a registered error subclass otherwise.
     def check_response_errors(response)
       if !response.is_a?(Hash)
         raise RocketPants::Error, "The response from the server was not in a supported format."
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -6,10 +6,19 @@ module WebmockResponses
   
   FIXTURE_ROOT = File.expand_path('../../fixtures/', __FILE__)
   
+  # Takes a named json fixture and returns the JSON content from
+  # the file in the fixture directory, suitable for mocking out
+  # the response or parsing for use with content checking.
+  # @param [String] response_file the name (minus .json) of the response file to use
   def api_fixture_json(response_file)
     File.read(File.join(FIXTURE_ROOT, "#{response_file}.json"))
   end
   
+  # Takes the response of stub_request from Webmock, a mock response name
+  # and some extra data to mock out what the response should return.
+  # @param [WebMock::RequestStub] api_stub the request stub to set the details on
+  # @param [String] response_file the file name to use to get the json contents
+  # @param [Hash] extra any extra options to pass to the to_return method on the api stub.
   def add_response_stub(api_stub, response_file, extra = {})
     json = api_fixture_json(response_file)
     api_stub.to_return({
@@ -19,8 +28,13 @@ module WebmockResponses
     }.merge(extra))
   end
   
-  def stub_with_fixture(method, url, response_file, extra = {})
-    add_response_stub stub_api(method, url), response_file, extra
+  # Shorthand to stub out an api call with the given details.
+  # @param [Symbol] method the http method to stub out or :any
+  # @param [String] path The path (relative to the api endpoint) to stub
+  # @param [String] response_file The name of the response file
+  # @param [Hash] extra any extra options to pass to the response stub
+  def stub_with_fixture(method, path, response_file, extra = {})
+    add_response_stub stub_api(method, path), response_file, extra
   end
   
 end
