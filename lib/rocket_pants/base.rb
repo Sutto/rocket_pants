1f252f8a01464dc3308dea7d8dbca884b8ebc745
Darcy Laycock <sutto@thefrontiergroup.com.au>
2011-02-21 16:39:54 +0800

    Add respond_to

--- lib/rocket_pants/base.rb
+++ lib/rocket_pants/base.rb
@@ -1,18 +1,18 @@
 require 'rocket_pants/exceptions'
 
 module RocketPants
-  
+
   autoload :Caching,         'rocket_pants/controller/caching'
   autoload :ErrorHandling,   'rocket_pants/controller/error_handling'
   autoload :Instrumentation, 'rocket_pants/controller/instrumentation'
   autoload :Respondable,     'rocket_pants/controller/respondable'
   autoload :Versioning,      'rocket_pants/controller/versioning'
   autoload :UrlFor,          'rocket_pants/controller/url_for'
-  
+
   class Base < ActionController::Metal
-    
+
     abstract!
-    
+
     MODULES = [
       ActionController::HideActions,
       ActionController::UrlFor,
@@ -30,11 +30,13 @@ module RocketPants
       ActionController::Rescue,
       ErrorHandling
     ]
-    
+
     MODULES.each do |mixin|
       include mixin
     end
-    
+
+    respond_to :json
+
     # Bug fix for rails - include compatibility.
     config_accessor :protected_instance_variables
     self.protected_instance_variables = %w(@assigns @performed_redirect @performed_render
@@ -42,6 +44,6 @@ module RocketPants
       @before_filter_chain_aborted @_headers @_params @_response)
 
     ActiveSupport.run_load_hooks(:rocket_pants, self)
-    
+
   end
 end
