ec0780bbdbd52f29fcb82041cad3e914fca0e774
Darcy Laycock <sutto@sutto.net>
2012-05-06 11:27:54 +0800

    Failing spec for caching

---
+++ spec/rocket_pants/cache_middleware_spec.rb
@@ -0,0 +1,22 @@
+require 'spec_helper'
+
+describe RocketPants::CacheMiddleware do
+
+  include ControllerHelpers
+
+  before :all do
+    controller_class.logger = Logger.new(StringIO.new)
+  end
+
+  # Wrap the normal request in cache middleware.
+  def request(action = :echo)
+    @request = Rack::MockRequest.new(RocketPants::CacheMiddleware.new(controller_class.action(action)))
+  end
+
+  it 'should return the correct status with the rails default etags' do
+    # Now, do the request
+    get(:echo, {:echo => "same thing"}, {}, 'HTTP_IF_NONE_MATCH' => '"6f5d89fd787b63b076227b8ddcd27e27"')
+    response.status.should == 200 # Not 304.
+  end
+
+end
