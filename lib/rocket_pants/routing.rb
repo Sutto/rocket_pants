a9d00f23a54667c4416b853be9ab97f3a36d49f2
Darcy Laycock <sutto@sutto.net>
2012-08-31 20:44:08 +0800

    Setup path / header versioning based on constraints. First stab at it.

--- lib/rocket_pants/routing.rb
+++ lib/rocket_pants/routing.rb
@@ -1,5 +1,22 @@
+require 'set'
+
 module RocketPants
   module Routing
+
+    class HeaderVersioningConstraint
+
+      def initialize(versions)
+        @versions = Set.new(versions)
+      end
+
+      def matches?(request)
+        accept_header = request.headers['Accept']
+        return if accept_header.blank?
+        match = RocketPants.compiled_version_header_regexp.match(accept_header)
+        match && @versions.include?(match[1])
+      end
+
+    end
 
     # Scopes a set of given api routes, allowing for option versions.
     # @param [Hash] options options to pass through to the route e.g. `:module`.
@@ -11,13 +28,22 @@ module RocketPants
       versions.each do |version|
         raise ArgumentError, "Got invalid version: '#{version}'" unless version =~ /\A\d+\Z/
       end
-      versions_regexp = /(#{versions.uniq.join("|")})/
       raise ArgumentError, 'please provide atleast one version' if versions.empty?
-      options = options.deep_merge({
-        :constraints => {:version => versions_regexp},
-        :path        => ':version',
-        :defaults    => {:format => 'json'}
-      })
+      if RocketPants.path_versioning?
+        versions_regexp = /(#{versions.uniq.join("|")})/
+        options = options.deep_merge({
+          :constraints => {:version => versions_regexp},
+          :path        => ':version',
+          :defaults    => {:format => 'json'}
+        })
+      elsif RocketPants.header_versioning?
+        options = options.deep_merge({
+          :constraints => {:version => HeaderVersioningConstraint.new(versions)},
+          :defaults    => {:format => 'json'}
+        })
+      else
+        raise "Something's broken batman - Neither path or header versioning."
+      end
       scope options, &blk
     end
     alias api rocket_pants
