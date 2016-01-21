c386e4785169b94f3d48c0430d065ae0679b9b0a
Anders Hassis <anders@spartasales.com>
2016-01-21 11:06:10 +0100

    Updated path

--- lib/rocket_pants/railtie.rb
+++ lib/rocket_pants/railtie.rb
@@ -7,7 +7,7 @@ module RocketPants
     config.rocket_pants.pass_through_errors = nil
     config.rocket_pants.pass_through_errors = nil
 
-    config.i18n.railties_load_path << File.expand_path('../locale/en.yml', __FILE__)
+    config.i18n.load_path << File.expand_path('../locale/en.yml', __FILE__)
 
     initializer "rocket_pants.logger" do
       ActiveSupport.on_load(:rocket_pants) { self.logger ||= Rails.logger }
