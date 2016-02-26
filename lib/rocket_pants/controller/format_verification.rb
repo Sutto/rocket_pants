08e74df26f018c6b99ca9d82da773e810609a89b
Anders Hassis <anders@spartasales.com>
2016-02-26 17:06:49 +0100

    Use before_action instead

--- lib/rocket_pants/controller/format_verification.rb
+++ lib/rocket_pants/controller/format_verification.rb
@@ -3,7 +3,7 @@ module RocketPants
     extend ActiveSupport::Concern
 
     included do
-      before_filter :ensure_has_valid_format
+      before_action :ensure_has_valid_format
     end
 
     private
